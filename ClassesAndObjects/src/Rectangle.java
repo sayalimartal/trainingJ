847ac999df6788d569748eb45dda6c5085e12e8a
sayalimartal <41475386+sayalimartal@users.noreply.github.com>
2018-07-23 00:18:18 +0530

    Update Rectangle.java

--- ClassesAndObjects/src/Rectangle.java
+++ ClassesAndObjects/src/Rectangle.java
@@ -16,13 +16,13 @@ public float getBreadth() {
 public void setBreadth(float breadth) {
 	this.breadth = breadth;
 }
-
+//Area of the rectangle
 public float area()
 {
 	float area=length*breadth;
 	return area;
 }
-
+//Perimeter of the rectangle
 public float perimeter()
 {
 	float perimeter=2*(length+breadth);
