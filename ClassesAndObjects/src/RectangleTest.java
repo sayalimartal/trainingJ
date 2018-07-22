bec729a5cca7d4284b0356cd8f305011e9580d30
sayalimartal <41475386+sayalimartal@users.noreply.github.com>
2018-07-23 00:44:23 +0530

    Update RectangleTest.java

--- ClassesAndObjects/src/RectangleTest.java
+++ ClassesAndObjects/src/RectangleTest.java
@@ -1,23 +1,48 @@
-
+import java.util.Scanner;
 public class RectangleTest {
 
 	public static void main(String[] args) {
 		
-		Rectangle rectangle=new Rectangle();
+		Rectangle rectangle1=new Rectangle();
+		Rectangle rectangle2=new Rectangle();
+
+		Scanner scanner=new Scanner(System.in);
+
+		System.out.println("Enter the length of the rectangle");
+		rectangle1.setLength(scanner.nextFloat());
+
+		System.out.println("Enter the breadth of the rectangle");
+		rectangle1.setBreadth(scanner.nextFloat());
 		
-		rectangle.setLength(10.5f);
-		rectangle.setBreadth(2.9f);
+		System.out.println("Length of the rectangle is "+rectangle1.getLength()); 
+		System.out.println("Breadth of the rectangle is "+rectangle1.getBreadth());
+
+		//Display the area and the perimeter of the rectangle
+		System.out.println("Area of the rectangle is "+rectangle1.area());
+		System.out.println("Perimeter of the rectangle is "+rectangle1.perimeter());
+
+		System.out.println("Is a second rectangle present? Yes/No");
+		String answer=scanner.next();
 		
-		float length=rectangle.getLength();
-		System.out.println("Length of the rectangle is "+length); 
+		if(answer.equals("Yes"))
+		{
+			System.out.println("Enter the length of the rectangle");
+			rectangle2.setLength(scanner.nextFloat());
+
+			System.out.println("Enter the breadth of the rectangle");
+			rectangle2.setBreadth(scanner.nextFloat());
 		
-		float breadth=rectangle.getBreadth();
-		System.out.println("Breadth of the rectangle is "+breadth);
+			System.out.println("Length of the rectangle is "+rectangle2.getLength()); 
+			System.out.println("Breadth of the rectangle is "+rectangle2.getBreadth());
+
+			//Display the area and the perimeter of the rectangle
+			System.out.println("Area of the rectangle is "+rectangle2.area());
+			System.out.println("Perimeter of the rectangle is "+rectangle2.perimeter());
 		
-		float area=rectangle.area();
-		System.out.println("Area of the rectangle is "+area);
-		
-		float perimeter=rectangle.perimeter();
-		System.out.println("Perimeter of the rectangle is "+perimeter);
+			if(rectangle1.area() == rectangle2.area())
+				System.out.println("The two rectangles have the same area");
+			else
+				System.out.println("TThe two rectangles have different areas");
+		}
 	}
 }
