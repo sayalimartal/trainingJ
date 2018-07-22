b50c7d762c2ad7a0361bfd2860a63c892e9ace69
sayalimartal <41475386+sayalimartal@users.noreply.github.com>
2018-07-22 23:40:57 +0530

    Update TriangleCheckTest.java

--- ClassesAndObjects2/src/TriangleCheckTest.java
+++ ClassesAndObjects2/src/TriangleCheckTest.java
@@ -4,11 +4,13 @@ public class TriangleCheckTest {
 
 	public static void main(String[] args) {
 		
-		Scanner sc=new Scanner(System.in);
 		int i;
+		Scanner sc=new Scanner(System.in);
+		
 		System.out.println("Enter the number of triangles");
 		int num=sc.nextInt();
-		int[] side1=new int[3];
+		
+		int[][] side1=new int[num][3];
 		Triangle[] triangle=new Triangle[num];
 		
 		for(i=0;i<num;i++)
@@ -17,11 +19,8 @@ public class TriangleCheckTest {
 			
 			System.out.println("Enter the sides of the triangle");
 			for(int j=0;j<3;j++)
-				side1[j]=sc.nextInt();
-				
-			
-			triangle[i].setSide(side1);
-			System.out.println(triangle[i].getSide());
+				side1[i][j]=sc.nextInt();
+			triangle[i].setSide(side1[i]);
 		}
 		
 		for(i=0;i<num;i++)
@@ -44,9 +43,6 @@ public class TriangleCheckTest {
 					}
 				}
 			}
-		}
-					
-				
-				
+		}			
 	}
 }
